805dbf641b421fa6417281d688eee465b2a3e98d
kel2203 <raquelsb2001@gmail.com>
2023-06-16 09:56:13 -0300

    first commit

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,32 @@
+<template>
+   <header>
+    <h1>
+        <img src="../assets/flower.png" alt="logo">
+    </h1>
+   </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'BarraLateral',
+});
+</script>
+
+<style scoped>
+header{
+    background-color: #5c2fff;
+    width: 100%;
+    height: 100vh;
+    padding: 1rem;
+    
+}
+
+@media screen and (max-width: 768px){
+    header{
+        height: 2.5rem;
+        height: auto;
+    }
+    
+}
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="columns is-8" role="form" aria-label="Formulario para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa voce deseja iniciar?">
+                
+            </div>
+            <div class="is-fles is-aling-items-center is-justify-content-space-between">
+            <div class="columns">
+               
+                    <section>
+                        <strong>
+                            {{ tempoDecorrido  }}
+                        </strong>
+                        
+                      </section>
+                      <button class="button" @click="iniciarTarefa">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button class="button" @click="pararTarefa">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>
+                        </div>
+                </div>
+         
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'FormularioMestre',
+    data(){
+        return{
+            tempoEmSegundos: 0,
+            cronometro: 0
+        }
+    },
+    computed: {
+        tempoDecorrido() : string {
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8);
+        }
+    },
+    methods: {
+
+       
+           
+        iniciarTarefa(){
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos++;
+            }, 1000);
+        },
+        pararTarefa(){
+           clearInterval(this.cronometro);
+        }
+    }
+});
+</script>
